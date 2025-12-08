33904a4263184a4118f272f689cce369e0549066
Tran Huynh An Duy (Andy) <36150380+andylovecloud@users.noreply.github.com>
2025-12-08 12:18:11 +0000

    day 10

--- lessons/day10/code/outputs.tf
+++ lessons/day10/code/outputs.tf
@@ -18,27 +18,27 @@ output "conditional_instance_id" {
 # ==============================================================================
 # Uncomment when testing Example 2
 
-# output "dynamic_sg_id" {
-#   description = "Security group ID with dynamic rules"
-#   value       = aws_security_group.dynamic_sg.id
-# }
+output "dynamic_sg_id" {
+  description = "Security group ID with dynamic rules"
+  value       = aws_security_group.dynamic_sg.id
+}
 
-# output "security_group_rules_count" {
-#   description = "Number of ingress rules created dynamically"
-#   value       = length(var.ingress_rules)
-# }
+output "security_group_rules_count" {
+  description = "Number of ingress rules created dynamically"
+  value       = length(var.ingress_rules)
+}
 
 # ==============================================================================
 # EXAMPLE 3 OUTPUTS: SPLAT EXPRESSION
 # ==============================================================================
 # Uncomment when testing Example 3
 
-# output "all_instance_ids" {
-#   description = "All instance IDs using splat expression [*]"
-#   value       = aws_instance.splat_example[*].id
-# }
+output "all_instance_ids" {
+  description = "All instance IDs using splat expression [*]"
+  value       = aws_instance.splat_example[*].id
+}
 
-# output "all_private_ips" {
-#   description = "All private IPs using splat expression [*]"
-#   value       = aws_instance.splat_example[*].private_ip
-# }
+output "all_private_ips" {
+  description = "All private IPs using splat expression [*]"
+  value       = aws_instance.splat_example[*].private_ip
+}
