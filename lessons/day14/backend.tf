1ac5b243629919f01d47fefbe5327b9712d816cb
Tran Huynh An Duy (Andy) <36150380+andylovecloud@users.noreply.github.com>
2025-12-10 10:16:54 +0000

    day14

--- lessons/day14/backend.tf
+++ lessons/day14/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "my-terraform-state-bucket-piyushsachdeva"
+    bucket = "my-terraform-state-bucket-andylovecloud"
     key    = "lessons/day14/terraform.tfstate"
-    region = "us-east-1"
+    region = "eu-north-1"
   }
 }
